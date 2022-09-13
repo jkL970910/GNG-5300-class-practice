e87b8420d09fc03d7897dbfd0c4dcc927286669b
JIEKUN LIU <61619839+jkL970910@users.noreply.github.com>
2022-09-13 19:34:17 -0400

    Create Class1Practice.py

---
+++ Class1Practice.py
@@ -0,0 +1,36 @@
+# GNG 5300 class 1 practice - Jiekun Liu
+
+# empty value =========================
+my_empty_value = None
+print(my_empty_value)
+
+# data type ===========================
+my_integer_variable = 1
+my_float_variable = 1.0
+my_complex_variable = 1 + 2j
+my_list_variable = [1,2]
+my_string_variavle = "string"
+print(type(my_integer_variable))
+print(type(my_float_variable))
+print(type(my_complex_variable))
+print(type(my_list_variable))
+
+# operation ===========================
+a = 2
+b = 3
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(a % b)
+
+# string concatenate ==================
+str1 = "abc"
+str2 = "def"
+print(str1 + str2)
+print("".join([str1, str2]))
+print("%s%s" % (str1, str2))
+print("{}{}".format(str1, str2))
+print(f"{str1}{str2}")
+
