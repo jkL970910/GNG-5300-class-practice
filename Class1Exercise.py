c9a2af6b8c0bfdde5219a9937fa86b987cab958a
HaomingJue <59827786+HaomingJue@users.noreply.github.com>
2022-09-13 19:57:28 -0400

    Update Class1Exercise.py

--- Class1Exercise.py
+++ Class1Exercise.py
@@ -1,10 +1,18 @@
-user_input_1 = input("please enter your first value: ")
-user_input_2 = input("please enter your second value: ")
+user_input_1 = float(input("please enter your first value: "))
+user_input_2 = float(input("please enter your second value: "))
+user_input_3 = float(input("please enter your third value:"))
 
 output_operation_1 = user_input_1 + user_input_2
 output_operation_2 = user_input_1 - user_input_2
 output_operation_3 = user_input_1 * user_input_2
 output_operation_4 = user_input_1 ** user_input_2
 output_operation_5 = user_input_1 / user_input_2
+print(output_operation_1, output_operation_2, output_operation_3, output_operation_4, output_operation_5)
 
-print(output_operation_1, output_operation_2, output_operation_2, output_operation_2, output_operation_2)
+output_operation_1 = output_operation_1 + user_input_3
+output_operation_2 = output_operation_2 - user_input_3
+output_operation_3 = output_operation_3 * user_input_3
+output_operation_4 = output_operation_4 ** user_input_3a
+output_operation_5 = output_operation_5 / user_input_3
+print(output_operation_1, output_operation_2, output_operation_3, output_operation_4, output_operation_5)
+
