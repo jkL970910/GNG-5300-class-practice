17d2d4d37b41840ae79abb2a53059763f27921c2
JIEKUN LIU <61619839+jkL970910@users.noreply.github.com>
2022-09-13 19:40:07 -0400

    Create Class1Exercise.py

---
+++ Class1Exercise.py
@@ -0,0 +1,10 @@
+user_input_1 = 2
+user_input_2 = 3
+
+output_operation_1 = user_input_1 + user_input_2
+output_operation_2 = user_input_1 - user_input_2
+output_operation_3 = user_input_1 * user_input_2
+output_operation_4 = user_input_1 ** user_input_2
+output_operation_5 = user_input_1 / user_input_2
+
+print(output_operation_1, output_operation_2, output_operation_2, output_operation_2, output_operation_2)
